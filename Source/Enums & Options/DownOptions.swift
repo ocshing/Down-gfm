32d952e9e91d5330f6c32d821026ffd52d983478
vvveiii <cntrump@gmail.com>
2018-11-29 22:19:06 +0800

    use cmark-gfm.

--- Source/Enums & Options/DownOptions.swift
+++ Source/Enums & Options/DownOptions.swift
@@ -30,31 +30,61 @@ public struct DownOptions: OptionSet {
     */
     public static let hardBreaks = DownOptions(rawValue: CMARK_OPT_HARDBREAKS)
 
-    /**
-     Suppress raw HTML and unsafe links (`javascript:`, `vbscript:`,
-     `file:`, and `data:`, except for `image/png`, `image/gif`,
-     `image/jpeg`, or `image/webp` mime types).  Raw HTML is replaced
-     by a placeholder HTML comment. Unsafe links are replaced by
-     empty strings.
-    */
-    public static let safe = DownOptions(rawValue: CMARK_OPT_SAFE)
+    /** Render `softbreak` elements as spaces.
+     */
+   public static let noBreaks = DownOptions(rawValue: CMARK_OPT_NOBREAKS)
 
     // MARK: - Parsing Options
 
     /**
-     Normalize tree by consolidating adjacent text nodes.
-    */
+     * ### Options affecting parsing
+     */
+
+    /** Legacy option (no effect).
+     */
     public static let normalize = DownOptions(rawValue: CMARK_OPT_NORMALIZE)
 
-    /**
-     Validate UTF-8 in the input before parsing, replacing illegal
-     sequences with the replacement character U+FFFD.
-    */
+    /** Validate UTF-8 in the input before parsing, replacing illegal
+     * sequences with the replacement character U+FFFD.
+     */
     public static let validateUTF8 = DownOptions(rawValue: CMARK_OPT_VALIDATE_UTF8)
 
-    /**
-     Convert straight quotes to curly, --- to em dashes, -- to en dashes.
-    */
+    /** Convert straight quotes to curly, --- to em dashes, -- to en dashes.
+     */
     public static let smart = DownOptions(rawValue: CMARK_OPT_SMART)
 
+    /** Use GitHub-style <pre lang="x"> tags for code blocks instead of <pre><code
+     * class="language-x">.
+     */
+    public static let githubPreLang = DownOptions(rawValue: CMARK_OPT_GITHUB_PRE_LANG)
+
+    /** Be liberal in interpreting inline HTML tags.
+     */
+    public static let liberalHTMLTag = DownOptions(rawValue: CMARK_OPT_LIBERAL_HTML_TAG)
+
+    /** Parse footnotes.
+     */
+    public static let footnotes = DownOptions(rawValue: CMARK_OPT_FOOTNOTES)
+
+    /** Only parse strikethroughs if surrounded by exactly 2 tildes.
+     * Gives some compatibility with redcarpet.
+     */
+    public static let strikethroughDoubleTilde = DownOptions(rawValue: CMARK_OPT_STRIKETHROUGH_DOUBLE_TILDE)
+
+    /** Use style attributes to align table cells instead of align attributes.
+     */
+    public static let tablePreferStyleAttributes = DownOptions(rawValue: CMARK_OPT_TABLE_PREFER_STYLE_ATTRIBUTES)
+
+    /** Include the remainder of the info string in code blocks in
+     * a separate attribute.
+     */
+    public static let fullInfoString = DownOptions(rawValue: CMARK_OPT_FULL_INFO_STRING)
+
+    /** Allow raw HTML and unsafe links, `javascript:`, `vbscript:`, `file:`, and
+     * all `data:` URLs -- by default, only `image/png`, `image/gif`, `image/jpeg`,
+     * or `image/webp` mime types are allowed. Without this option, raw HTML is
+     * replaced by a placeholder HTML comment, and unsafe links are replaced by
+     * empty strings.
+     */
+    public static let unsafe = DownOptions(rawValue: CMARK_OPT_UNSAFE)
 }
---
+++ Source/cmark/libcmark.h
@@ -0,0 +1,18 @@
+//
+//  libcmark.h
+//  Down
+//
+//  Created by vvveiii on 2018/11/29.
+//  Copyright © 2018 Glazed Donut, LLC. All rights reserved.
+//
+
+#ifndef libcmark_h
+#define libcmark_h
+
+#include "src/node.h"
+#include "src/parser.h"
+#include "src/syntax_extension.h"
+#include "src/cmark-gfm.h"
+#include "extensions/cmark-gfm-core-extensions.h"
+
+#endif /* libcmark_h */
